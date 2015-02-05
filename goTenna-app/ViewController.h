3c8b0ce966de76b07dcebc445ac5f5cde7a1b807
Julie Yaunches <jmyaunch@gmail.com>
2015-02-05 17:39:18 -0500

    baseline project.. adding cryptopp library

---
+++ goTenna-app/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  goTenna-app
+//
+//  Created by Julietta Yaunches on 2/5/15.
+//  Copyright (c) 2015 Julietta Yaunches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ goTenna-appTests/goTenna_appTests.m
@@ -0,0 +1,40 @@
+//
+//  goTenna_appTests.m
+//  goTenna-appTests
+//
+//  Created by Julietta Yaunches on 2/5/15.
+//  Copyright (c) 2015 Julietta Yaunches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface goTenna_appTests : XCTestCase
+
+@end
+
+@implementation goTenna_appTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
