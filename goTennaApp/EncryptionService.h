6316cb77100803a2ead7967ce5d03c78e1611453
Julie Yaunches <jmyaunch@gmail.com>
2015-02-06 12:04:37 -0500

    Moving encryption stuff to encryption service class

---
+++ goTennaApp/EncryptionService.h
@@ -0,0 +1,14 @@
+//
+//  EncryptionService.h
+//  goTennaApp
+//
+//  Created by Julietta Yaunches on 2/6/15.
+//  Copyright (c) 2015 Julietta Yaunches. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EncryptionService : NSObject
+
+- (void)doStuff;
+@end
